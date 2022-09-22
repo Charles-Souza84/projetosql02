c6654368f2a47d94bc25ff1c5f69c9f1ac5d9b45
CharleSouza84 <charles.guara84@gmail.com>
2022-09-22 11:22:09 -0300

    atualizando arquivos sql e png

--- locadoraMySQL.sql
+++ locadoraMySQL.sql
@@ -1,101 +1,81 @@
-CREATE TABLE [generos] (
-  [id] int PRIMARY KEY,
-  [genero] nvarchar(255) NOT NULL
-)
-GO
+CREATE TABLE `generos` (
+  `id` int PRIMARY KEY,
+  `genero` varchar(255) NOT NULL
+);
 
-CREATE TABLE [filmes] (
-  [id] int PRIMARY KEY,
-  [titulo] nvarchar(255) NOT NULL,
-  [id_genero] int NOT NULL,
-  [valor] decimal(8,2) NOT NULL
-)
-GO
+CREATE TABLE `filmes` (
+  `id` int PRIMARY KEY,
+  `titulo` varchar(255) NOT NULL,
+  `id_genero` int NOT NULL,
+  `valor` decimal(8,2) NOT NULL
+);
 
-CREATE TABLE [dvds] (
-  [id] int PRIMARY KEY,
-  [id_filme] int NOT NULL,
-  [quantidade] int NOT NULL
-)
-GO
+CREATE TABLE `dvds` (
+  `id` int PRIMARY KEY,
+  `id_filme` int NOT NULL,
+  `quantidade` int NOT NULL
+);
 
-CREATE TABLE [atores] (
-  [id] int PRIMARY KEY,
-  [nome] nvarchar(255) NOT NULL
-)
-GO
+CREATE TABLE `atores` (
+  `id` int PRIMARY KEY,
+  `nome` varchar(255) NOT NULL
+);
 
-CREATE TABLE [atores_filme] (
-  [id] int PRIMARY KEY,
-  [id_filme] int NOT NULL,
-  [id_ator] int NOT NULL,
-  [personagem] nvarchar(255) NOT NULL
-)
-GO
+CREATE TABLE `atores_filme` (
+  `id` int PRIMARY KEY,
+  `id_filme` int NOT NULL,
+  `id_ator` int NOT NULL,
+  `personagem` varchar(255) NOT NULL
+);
 
-CREATE TABLE [clientes] (
-  [id] int PRIMARY KEY,
-  [nome] nvarchar(255) NOT NULL,
-  [sobrenome] nvarchar(255) NOT NULL,
-  [telefone] nvarchar(255) NOT NULL,
-  [endereco] nvarchar(255) NOT NULL
-)
-GO
+CREATE TABLE `clientes` (
+  `id` int PRIMARY KEY,
+  `nome` varchar(255) NOT NULL,
+  `sobrenome` varchar(255) NOT NULL,
+  `telefone` varchar(255) NOT NULL,
+  `endereco` varchar(255) NOT NULL
+);
 
-CREATE TABLE [emprestimos] (
-  [id] int PRIMARY KEY,
-  [data] date NOT NULL,
-  [id_cliente] int NOT NULL
-)
-GO
+CREATE TABLE `emprestimos` (
+  `id` int PRIMARY KEY,
+  `data` date NOT NULL,
+  `id_cliente` int NOT NULL
+);
 
-CREATE TABLE [filmes_emprestimo] (
-  [id] int PRIMARY KEY,
-  [id_emprestimo] int NOT NULL,
-  [id_filme] int NOT NULL
-)
-GO
+CREATE TABLE `filmes_emprestimo` (
+  `id` int PRIMARY KEY,
+  `id_emprestimo` int NOT NULL,
+  `id_dvd` int NOT NULL
+);
 
-CREATE TABLE [devolucoes] (
-  [id] int PRIMARY KEY,
-  [id_emprestimo] int NOT NULL,
-  [data] datetime NOT NULL
-)
-GO
+CREATE TABLE `devolucoes` (
+  `id` int PRIMARY KEY,
+  `id_emprestimo` int NOT NULL,
+  `data` datetime NOT NULL
+);
 
-CREATE TABLE [filmes_devolucao] (
-  [id] int PRIMARY KEY,
-  [id_devolucao] int NOT NULL,
-  [id_filme_emprestimo] int NOT NULL
-)
-GO
+CREATE TABLE `filmes_devolucao` (
+  `id` int PRIMARY KEY,
+  `id_devolucao` int NOT NULL,
+  `id_filme_emprestimo` int NOT NULL
+);
 
-ALTER TABLE [filmes] ADD FOREIGN KEY ([id_genero]) REFERENCES [generos] ([id])
-GO
+ALTER TABLE `filmes` ADD FOREIGN KEY (`id_genero`) REFERENCES `generos` (`id`);
 
-ALTER TABLE [dvds] ADD FOREIGN KEY ([id_filme]) REFERENCES [filmes] ([id])
-GO
+ALTER TABLE `dvds` ADD FOREIGN KEY (`id_filme`) REFERENCES `filmes` (`id`);
 
-ALTER TABLE [atores_filme] ADD FOREIGN KEY ([id_filme]) REFERENCES [filmes] ([id])
-GO
+ALTER TABLE `atores_filme` ADD FOREIGN KEY (`id_filme`) REFERENCES `filmes` (`id`);
 
-ALTER TABLE [atores_filme] ADD FOREIGN KEY ([id_ator]) REFERENCES [atores] ([id])
-GO
+ALTER TABLE `atores_filme` ADD FOREIGN KEY (`id_ator`) REFERENCES `atores` (`id`);
 
-ALTER TABLE [emprestimos] ADD FOREIGN KEY ([id_cliente]) REFERENCES [clientes] ([id])
-GO
+ALTER TABLE `emprestimos` ADD FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`id`);
 
-ALTER TABLE [filmes_emprestimo] ADD FOREIGN KEY ([id_emprestimo]) REFERENCES [emprestimos] ([id])
-GO
+ALTER TABLE `filmes_emprestimo` ADD FOREIGN KEY (`id_emprestimo`) REFERENCES `emprestimos` (`id`);
 
-ALTER TABLE [filmes_emprestimo] ADD FOREIGN KEY ([id_filme]) REFERENCES [filmes] ([id])
-GO
+ALTER TABLE `filmes_emprestimo` ADD FOREIGN KEY (`id_dvd`) REFERENCES `dvds` (`id`);
 
-ALTER TABLE [devolucoes] ADD FOREIGN KEY ([id_emprestimo]) REFERENCES [emprestimos] ([id])
-GO
+ALTER TABLE `devolucoes` ADD FOREIGN KEY (`id_emprestimo`) REFERENCES `emprestimos` (`id`);
 
-ALTER TABLE [filmes_devolucao] ADD FOREIGN KEY ([id_devolucao]) REFERENCES [devolucoes] ([id])
-GO
+ALTER TABLE `filmes_devolucao` ADD FOREIGN KEY (`id_devolucao`) REFERENCES `devolucoes` (`id`);
 
-ALTER TABLE [filmes_devolucao] ADD FOREIGN KEY ([id_filme_emprestimo]) REFERENCES [filmes_emprestimo] ([id])
-GO
+ALTER TABLE `filmes_devolucao` ADD FOREIGN KEY (`id_filme_emprestimo`) REFERENCES `filmes_emprestimo` (`id`);
